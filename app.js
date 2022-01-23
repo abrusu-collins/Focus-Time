a10acda73277f8eba77b14ec95faaffb8a17da62
abrusu-collins <snillochemingson@gmail.com>
2022-01-22 19:43:52 -0800

    Perfect

--- app.js
+++ app.js
@@ -1,58 +1,98 @@
-let timediv=document.getElementById("timediv");
-let timeofday=document.getElementById("timeofday");
-
+let timediv = document.getElementById("timediv");
+let timeofday = document.getElementById("timeofday");
+let namediv = document.getElementById("name");
+let focusdiv = document.getElementById("focus");
 //getTime function
-function getTime(){
-    //get time
-    let time=new Date();
-let secs=time.getSeconds().toString().length==1?"0"+time.getSeconds():time.getSeconds();
-let mins=time.getMinutes().toString().length==1?"0"+time.getMinutes():time.getMinutes();
-let hours=time.getHours().toString().length==1?"0"+time.getHours():time.getHours();
+function getTime() {
+  //get time
+  let time = new Date();
+  let secs =
+    time.getSeconds().toString().length == 1
+      ? "0" + time.getSeconds()
+      : time.getSeconds();
+  let mins =
+    time.getMinutes().toString().length == 1
+      ? "0" + time.getMinutes()
+      : time.getMinutes();
+  let hours =
+    time.getHours().toString().length == 1
+      ? "0" + time.getHours()
+      : time.getHours();
 
+  //AM or PM
+  if (hours >= 12) {
+    var amPm = "PM";
+  } else {
+    var amPm = "AM";
+  }
+  timediv.textContent = `${hours}: ${mins}: ${secs} ${amPm}`;
+  //Morning, Afternoon or Evening
+  if (hours >= 0 && hours < 12) {
+    timeofday.textContent = `Morning`;
+  } else if (hours >= 12 && hours < 18) {
+    timeofday.textContent = `Afternoon`;
+  } else {
+    timeofday.textContent = `Evening`;
+  }
 
-
-//AM or PM
-if(hours>=12){
-    var amPm="PM";
-}else{
-    var amPm="AM";
-}
-timediv.textContent=`${hours}: ${mins}: ${secs} ${amPm}`;
-//Morning, Afternoon or Evening
-if(hours>=0 &&hours <12){
-    timeofday.textContent=`Morning`;
-}else if(hours>=12 &&hours<18){
-    timeofday.textContent=`Afternoon`;
-
-}
-else{
-    timeofday.textContent=`Evening`;
-
-}
-
-//background changing
-if(hours>=6 &&hours<12){
-    document.body.classList.remove("midnight")
+  //background changing
+  if (hours >= 6 && hours < 12) {
+    document.body.classList.remove("midnight");
     document.body.classList.add("morning");
-}
-else if(hours>=12 &&hours<18){
+  } else if (hours >= 12 && hours < 18) {
     document.body.classList.remove("morning");
     document.body.classList.add("afternoon");
-}
-else if(hours>=18 &&hours<=23){
+  } else if (hours >= 18 && hours <= 23) {
     document.body.classList.remove("afternoon");
     document.body.classList.add("night");
-}
-
-else if(hours>=0 &&hours<6){
+  } else if (hours >= 0 && hours < 6) {
     document.body.classList.remove("night");
     document.body.classList.add("midnight");
-}
-//call itself every one second
-setInterval(()=>{getTime()},1000);
-
-
+  }
+  //call itself every one second
+  setInterval(() => {
+    getTime();
+  }, 1000);
 }
 
 getTime();
 
+//local Storage for name
+
+namediv.addEventListener("keydown", storename, true);
+function storename(e) {
+  if (e.which == 13 || e.keyCode == 13) {
+    e.preventDefault();
+    let name = namediv.textContent;
+    localStorage.setItem("name", name);
+    namediv.textContent = localStorage.getItem("name");
+  }
+}
+
+//local Storage for focus
+
+focusdiv.addEventListener("keydown", storefocus, true);
+function storefocus(e) {
+  if (e.which == 13 || e.keyCode == 13) {
+    e.preventDefault();
+    let focus = focusdiv.textContent;
+
+    localStorage.setItem("focus", focus);
+    focusdiv.textContent = localStorage.getItem("focus");
+  }
+}
+
+//fetching when reloaded
+window.onload = function (e) {
+  let name =
+    localStorage.getItem("name") == null
+      ? "[Enter name]"
+      : localStorage.getItem("name");
+  namediv.textContent = `${name}`;
+
+  let focus =
+    localStorage.getItem("focus") == null
+      ? "[Enter focus]"
+      : localStorage.getItem("focus");
+  focusdiv.textContent = `${focus}`;
+};
